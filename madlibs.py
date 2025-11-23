7a3d7a276bcf73c8458fbf7e13e43342d28fc135
kian <curagoned@gmail.com>
2025-11-23 17:11:04 +0330

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# Mad libs Game
+
+# user inputs
+adjective1 = input("Enter an adjective (description):")
+noun1 = input("Enter a noun (person, place or thing):")
+adjective2 = input("Enter an adjective (description):")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective (description):")
+
+# story output
+print("**Your Mad Libs story**")
+print(f"Today I went to a {adjective1} village.")
+print(f"In that place I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
+print("**The End**")
